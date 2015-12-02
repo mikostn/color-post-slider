e3f49d2a52d73197edac6b795959986be6c3bdc1
miko <web.m@stn.pl>
2015-12-02 20:50:33 +0100

    add color bars animation

--- assets/js/color-post-slider.js
+++ assets/js/color-post-slider.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function($) {
 	$('.colorPost').click(function() {
 		$(this).toggleClass('opened');
 		$(this).closest('.colorPosts').toggleClass('opened');
-		$(this).closest('colorPostSlider').toggleClass('opened');
+		$(this).closest('.colorPostSlider').toggleClass('opened');
 	});
 })
