d8abdadc1704aeb7841023ffd4c479656430010f
miko <web.m@stn.pl>
2015-12-09 13:54:37 +0100

    initial viewport size units support addresses #2

--- assets/js/color-post-slider.js
+++ assets/js/color-post-slider.js
@@ -1,30 +1,32 @@
-jQuery(document).ready(function($) {
-    
-/* TO DO:
- * 
- * -set elements width and height to fix current screen size:
- *      $(window).on("resize", repositionDivOnResize)
- * - touch support
- */
+jQuery(document).ready(function ($) {
 
-            // First remove all opened, if any...
-	$('.color, .colorPost, .colorPosts, .colorPostSlider').removeClass('opened');
+    /* TO DO:
+     * 
+     * -set elements width and height to fix current screen size:
+     *      $(window).on("resize", repositionDivOnResize)
+     * - touch support
+     */
 
-	// set colors height to match slider
-	$('.color, .colorPost, .colorPosts').height($('.colorPostSlider').height());
+    // First remove all opened, if any...
+    $('.color, .colorPost, .colorPosts, .colorPostSlider').removeClass('opened');
+//$(window).on("resize"
 
-	$('.color, .colorPost').click(function() {
-		// get clicked color slide
-		$colorSlide = $('[data-color='+$(this).data('color')+']');
-		// toggle clicked slide state
-		$colorSlide.toggleClass('opened');
-		// is anything opened?
-		$ifOpened = $('.color, .colorPost').hasClass('opened');
-		// close all other slides if any opend
-		if($ifOpened){
-			$colorSlide.siblings().removeClass('opened');
-		}
-		// toggle parents state
-		$colorSlide.closest('.colorPostSlider, .colorPosts').toggleClass('opened', $ifOpened);
-	});
+    $('.color, .colorPost').click(function () {
+        // set colors height to match slider
+//	$('.color, .colorPost, .colorPosts').height($('.colorPostSlider').height());
+        $('.color, .colorPost, .colorPosts').height($('.colorPost .imageContainer img').height());
+
+        // get clicked color slide
+        $colorSlide = $('[data-color=' + $(this).data('color') + ']');
+        // toggle clicked slide state
+        $colorSlide.toggleClass('opened');
+        // is anything opened?
+        $ifOpened = $('.color, .colorPost').hasClass('opened');
+        // close all other slides if any opend
+        if ($ifOpened) {
+            $colorSlide.siblings().removeClass('opened');
+        }
+        // toggle parents state
+        $colorSlide.closest('.colorPostSlider, .colorPosts').toggleClass('opened', $ifOpened);
+    });
 });
